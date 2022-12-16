daf5207e22d7585d6d517546c823f551aab631ad
Marcos Vinícius <101370923+DeVinc1@users.noreply.github.com>
2022-12-16 13:26:56 -0300

    Criando arquivo com os comandos SQL executados
    Criando um arquivo que sumarize os comandos SQL utilizados dentro classe MySQL_manipulação para integrar o app em java com um banco de dados

---
+++ src/SQLcomandos.sql
@@ -0,0 +1,28 @@
+/* Arquivo com todos os comandos SQL utilizados no projeto */
+
+/*
+ * Criação da tabela do orçamento do projeto:
+ *
+ * É necessário alterar o parametros `data_base` para o nome da base de dados conectada para o funcionamento correto do 
+ * comando no MySQL e concetenar o nome do projeto ao nome da tabela para torná-la única.
+ *
+ */
+ 
+CREATE TABLE `data_base`.`orçamento_string` 
+      ( `nome_Item` VARCHAR(100) NOT NULL, 
+      `complexidade_Item` INT NOT NULL, 
+      `tipo_Item` VARCHAR(45) NOT NULL, 
+      `horasGastas_Item` INT NOT NULL, 
+      `valor_Item` DOUBLE NOT NULL,
+      PRIMARY KEY (`ID_item`));
+     
+     
+/*
+ * Inserção de dados dentro da tabela
+ *
+ * É necessário mudar os parâmetros passados em VALUE ou utilizar o comando .setString() no Java
+ * para o funcionamento do comando dentro do MySQL
+ *
+ */
+ 
+ INSERT INTO `data_base`.`orçamento_" + itemDoSistema.getProjetoPai() + "` (`nome_Item`, `complexidade_Item`, `tipo_Item`, `horasGastas_Item`, `valor_Item`) VALUES (?, ?, ?, ?, ?);
